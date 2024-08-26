cfa8053134a86d0406fc9cafa17b0a17b6036ec4
leewwwhui <leewwhui@gmail.com>
2024-08-26 13:19:28 +1000

    🎨 update layout

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -1,17 +1,19 @@
 import { FC } from "react";
-import { SiteHeader } from "./site-header";
 import { Button } from "../ui/button";
 
 export const MainLayout: FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="w-full h-screen pt-20">
-      <div className="w-full fixed left-0 top-0 z-30 bg-background shadow">
-        <SiteHeader />
-      </div>
+      <div className="w-full fixed left-0 top-0 z-30 shadow h-20 bg-orange-300"></div>
 
-      <div className="w-full h-full pl-[300px]">
-        <div className="flex fixed left-0 top-0 w-[250px] pt-20">
-          <Button>hello world</Button>
+      <div className="w-full min-h-full pl-80 relative">
+        <div className="flex fixed left-0 top-0 pt-24 pl-6">
+          <div className="flex flex-col gap-3">
+            <Button>hello world</Button>
+            <Button>hello world</Button>
+            <Button>hello world</Button>
+            <Button>hello world</Button>
+          </div>
         </div>
         <div className="z-10 flex-1 p-4">{children}</div>
       </div>
